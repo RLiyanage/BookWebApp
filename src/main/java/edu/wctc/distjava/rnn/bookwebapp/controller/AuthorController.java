ca7b7d57db35a4e18b75370c6e9c91135f24a394
Rasika <Rasika@192.168.2.9>
2017-09-19 20:27:05 -0500

    First Commit

--- src/main/java/edu/wctc/distjava/rnn/bookwebapp/controller/AuthorController.java
+++ src/main/java/edu/wctc/distjava/rnn/bookwebapp/controller/AuthorController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.wctc.distjava.rnn.controller;
+package edu.wctc.distjava.rnn.bookwebapp.controller;
 
 import edu.wctc.distjava.rnn.bookwebapp.model.Author;
 import edu.wctc.distjava.rnn.bookwebapp.model.AuthorService;
